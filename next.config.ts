a1a2a72e4961c2d8b913711d9686b755fc089585
mehdibh <mehdibhaddou@gmail.com>
2026-03-02 20:29:55 +0200

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
-import { resolve } from "path";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: resolve(__dirname),
-  },
   async headers() {
     return [
       {
